e99fb17b5a689289a6bec72889e91110960b2b73
taiga kawakubo <kawakubo.t.14@gmail.com>
2026-07-21 16:20:50 +0900

    feat: implement authorization policies for books and reviews

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Book;
+use App\Models\Review;
+use App\Policies\BookPolicy;
+use App\Policies\ReviewPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +17,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Book::class => BookPolicy::class,
+        Review::class => ReviewPolicy::class,
     ];
 
     /**
