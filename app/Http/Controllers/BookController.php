5ab2dd704a665316b509edc155979c424dcc2486
taiga kawakubo <kawakubo.t.14@gmail.com>
2026-07-22 16:08:57 +0900

    feat: implement book registration

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreBookRequest;
 use Illuminate\Http\Request;
 use App\Models\Book;
+use App\Models\Genre;
 use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
 
 class BookController extends Controller
 {
@@ -17,6 +21,38 @@ class BookController extends Controller
         
         return view('books.index', compact('books'));
     }
+
+    public function create():View
+    {
+        $genres = Genre::query()->get();
+
+        return view('books.create', compact('genres'));
+    }
+
+
+    public function store(StoreBookRequest $request):RedirectResponse
+    {
+        $validated = $request->validated();
+
+        DB::transaction(function() use($request, $validated){
+            $book = Book::create([
+            'user_id' => $request->user()->id,
+            'title' => $validated['title'],
+            'author'=> $validated['author'],
+            'isbn' => $validated['isbn'],
+            'published_date' => $validated['published_date'],
+            'description' => $validated['description'] ?? null,
+            'image_url' => $validated['image_url'] ?? null,
+            ]);
+
+            $book->genres()->attach($validated['genres']);
+        });
+
+        return redirect()
+            ->route('books.index')
+            ->with('success', '書籍を登録しました。');
+    }
+
 
     public function show(Book $book): View
     {
